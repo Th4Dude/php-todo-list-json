38553582dccc774cc7b75e45abfde291338ef5cc
Th4Dude <gallottimaurizio@yahoo.it>
2023-04-12 20:14:49 +0300

    Chiamata ed aggiunta in html

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const { createApp } = Vue;
 
 createApp({
 
-
-
-
-    
+    data() {
+        return {
+            todos: []
+        }
+    },
+    mounted() {
+        axios.get('file.json')
+            .then(response => {
+                this.todos = response.data;
+            });
+    }
 })
 
 .mount('#app');
